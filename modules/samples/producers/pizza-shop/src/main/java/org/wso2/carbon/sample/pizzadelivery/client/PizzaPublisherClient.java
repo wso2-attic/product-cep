fc9e03daed0463fbe798bb2650768c1c67bbf47c
suhothayan <suhothayan@gmail.com>
2014-08-22 17:52:21 +0530

    Integrating rat and adding licence headers

--- modules/samples/producers/pizza-shop/src/main/java/org/wso2/carbon/sample/pizzadelivery/client/PizzaPublisherClient.java
+++ modules/samples/producers/pizza-shop/src/main/java/org/wso2/carbon/sample/pizzadelivery/client/PizzaPublisherClient.java
@@ -1,5 +1,5 @@
-package org.wso2.carbon.sample.pizzadelivery.client;/*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+/*
+*  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 *
 *  WSO2 Inc. licenses this file to you under the Apache License,
 *  Version 2.0 (the "License"); you may not use this file except
@@ -15,7 +15,7 @@ package org.wso2.carbon.sample.pizzadelivery.client;/*
 * specific language governing permissions and limitations
 * under the License.
 */
-
+package org.wso2.carbon.sample.pizzadelivery.client;
 
 import org.wso2.carbon.databridge.agent.thrift.DataPublisher;
 import org.wso2.carbon.databridge.agent.thrift.exception.AgentException;
