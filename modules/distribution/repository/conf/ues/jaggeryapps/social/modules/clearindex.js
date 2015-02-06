9627b57091785705767041222213bd3c834ba180
suhothayan <suhothayan@gmail.com>
2015-02-06 20:45:56 +0530

    removing ues from CEP

--- modules/distribution/repository/conf/ues/jaggeryapps/social/modules/clearindex.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var sso_sessions = application.get('sso_sessions');
-        if(!sso_sessions) {
-        return;
-    }
-	l = new Log();
-    l.debug("session deleting :: " + session.getId() + " :: " + sso_sessions[session.getId()]);
-    delete sso_sessions[session.getId()];
-}());
