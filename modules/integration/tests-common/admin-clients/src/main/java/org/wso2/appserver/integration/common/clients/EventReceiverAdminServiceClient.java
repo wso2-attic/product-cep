3a7e6589eb8a3a6840a3d9c8401ae8f584ed9b46
dilini <dilini@wso2.com>
2015-03-19 11:23:26 +0530

    Updating copyright

--- modules/integration/tests-common/admin-clients/src/main/java/org/wso2/appserver/integration/common/clients/EventReceiverAdminServiceClient.java
+++ modules/integration/tests-common/admin-clients/src/main/java/org/wso2/appserver/integration/common/clients/EventReceiverAdminServiceClient.java
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.wso2.appserver.integration.common.clients;
 
 import org.apache.axis2.AxisFault;
