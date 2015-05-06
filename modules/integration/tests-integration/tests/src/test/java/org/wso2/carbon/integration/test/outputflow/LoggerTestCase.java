3f1460392b05cd787494ea74a5b3b562c9699892
ramindu90 <ramindus@wso2.com>
2015-05-06 08:48:44 +0530

    Adding samples and integration tests for logger publisher.

---
+++ modules/integration/tests-integration/tests/src/test/java/org/wso2/carbon/integration/test/outputflow/LoggerTestCase.java
@@ -0,0 +1,292 @@
+/*
+*  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.integration.test.outputflow;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.event.simulator.stub.types.EventDto;
+import org.wso2.cep.integration.common.utils.CEPIntegrationTest;
+
+/**
+ * Testing Logger publisher in different formats (text, xml, json)
+ */
+public class LoggerTestCase extends CEPIntegrationTest {
+
+    private static final Log log = LogFactory.getLog(LoggerTestCase.class);
+
+    @BeforeClass(alwaysRun = true)
+    public void init()
+            throws Exception {
+
+        super.init(TestUserMode.SUPER_TENANT_ADMIN);
+        String loggedInSessionCookie = getSessionCookie();
+
+        eventSimulatorAdminServiceClient = configurationUtil.getEventSimulatorAdminServiceClient(backendURL, loggedInSessionCookie);
+        eventStreamManagerAdminServiceClient = configurationUtil.getEventStreamManagerAdminServiceClient(backendURL, loggedInSessionCookie);
+        eventPublisherAdminServiceClient = configurationUtil.getEventPublisherAdminServiceClient(backendURL, loggedInSessionCookie);
+    }
+
+    @Test(groups = {"wso2.cep"}, description = "Testing Text Logger")
+    public void loggerTextTestScenario() throws Exception {
+        int startESCount = eventStreamManagerAdminServiceClient.getEventStreamCount();
+        int startEPCount = eventPublisherAdminServiceClient.getActiveEventPublisherCount();
+
+        //Add StreamDefinition
+        String streamDefinitionAsString = getJSONArtifactConfiguration("outputflows/sample0055", "org.wso2.event.statistics.stream_1.0.0.json");
+        eventStreamManagerAdminServiceClient.addEventStreamAsString(streamDefinitionAsString);
+        Assert.assertEquals(eventStreamManagerAdminServiceClient.getEventStreamCount(), startESCount + 1);
+
+        //Add Text Logger
+        String eventPublisherConfig = getXMLArtifactConfiguration("outputflows/sample0055", "logger.xml");
+        eventPublisherAdminServiceClient.addEventPublisherConfiguration(eventPublisherConfig);
+        Assert.assertEquals(eventPublisherAdminServiceClient.getActiveEventPublisherCount(), startEPCount + 1);
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto.setAttributeValues(new String[]{"199008131245", "false", "100", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto2 = new EventDto();
+        eventDto2.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto2.setAttributeValues(new String[]{"199008131245", "false", "101", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto3 = new EventDto();
+        eventDto3.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto3.setAttributeValues(new String[]{"199008131245", "false", "103", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        eventSimulatorAdminServiceClient.sendEvent(eventDto);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto2);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto3);
+
+        eventStreamManagerAdminServiceClient.removeEventStream("org.wso2.event.statistics.stream", "1.0.0");
+        eventPublisherAdminServiceClient.removeInactiveEventPublisherConfiguration("logger.xml");
+
+        Thread.sleep(2000);
+    }
+
+    @Test(groups = {"wso2.cep"}, description = "Testing Text Logger with mapping")
+    public void loggerTextWithMappingTestScenario() throws Exception {
+        int startESCount = eventStreamManagerAdminServiceClient.getEventStreamCount();
+        int startEPCount = eventPublisherAdminServiceClient.getActiveEventPublisherCount();
+
+        //Add StreamDefinition
+        String streamDefinitionAsString = getJSONArtifactConfiguration("outputflows/sample0056", "org.wso2.event.statistics.stream_1.0.0.json");
+        eventStreamManagerAdminServiceClient.addEventStreamAsString(streamDefinitionAsString);
+        Assert.assertEquals(eventStreamManagerAdminServiceClient.getEventStreamCount(), startESCount + 1);
+
+        //Add Text Logger
+        String eventPublisherConfig = getXMLArtifactConfiguration("outputflows/sample0056", "logger.xml");
+        eventPublisherAdminServiceClient.addEventPublisherConfiguration(eventPublisherConfig);
+        Assert.assertEquals(eventPublisherAdminServiceClient.getActiveEventPublisherCount(), startEPCount + 1);
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto.setAttributeValues(new String[]{"199008131245", "false", "200", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto2 = new EventDto();
+        eventDto2.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto2.setAttributeValues(new String[]{"199008131245", "false", "201", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto3 = new EventDto();
+        eventDto3.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto3.setAttributeValues(new String[]{"199008131245", "false", "203", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        eventSimulatorAdminServiceClient.sendEvent(eventDto);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto2);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto3);
+
+        eventStreamManagerAdminServiceClient.removeEventStream("org.wso2.event.statistics.stream", "1.0.0");
+        eventPublisherAdminServiceClient.removeInactiveEventPublisherConfiguration("logger.xml");
+
+        Thread.sleep(2000);
+
+    }
+
+    @Test(groups = {"wso2.cep"}, description = "Testing JSON Logger")
+    public void loggerJSONTestScenario() throws Exception {
+        int startESCount = eventStreamManagerAdminServiceClient.getEventStreamCount();
+        int startEPCount = eventPublisherAdminServiceClient.getActiveEventPublisherCount();
+
+        //Add StreamDefinition
+        String streamDefinitionAsString = getJSONArtifactConfiguration("outputflows/sample0051", "org.wso2.event.statistics.stream_1.0.0.json");
+        eventStreamManagerAdminServiceClient.addEventStreamAsString(streamDefinitionAsString);
+        Assert.assertEquals(eventStreamManagerAdminServiceClient.getEventStreamCount(), startESCount + 1);
+
+        //Add Text Logger
+        String eventPublisherConfig = getXMLArtifactConfiguration("outputflows/sample0051", "logger.xml");
+        eventPublisherAdminServiceClient.addEventPublisherConfiguration(eventPublisherConfig);
+        Assert.assertEquals(eventPublisherAdminServiceClient.getActiveEventPublisherCount(), startEPCount + 1);
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto.setAttributeValues(new String[]{"199008131245", "false", "300", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto2 = new EventDto();
+        eventDto2.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto2.setAttributeValues(new String[]{"199008131245", "false", "301", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto3 = new EventDto();
+        eventDto3.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto3.setAttributeValues(new String[]{"199008131245", "false", "303", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        eventSimulatorAdminServiceClient.sendEvent(eventDto);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto2);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto3);
+
+        eventStreamManagerAdminServiceClient.removeEventStream("org.wso2.event.statistics.stream", "1.0.0");
+        eventPublisherAdminServiceClient.removeInactiveEventPublisherConfiguration("logger.xml");
+
+        Thread.sleep(2000);
+
+    }
+
+    @Test(groups = {"wso2.cep"}, description = "Testing JSON Mapping Logger")
+    public void loggerJSONWithMappingTestScenario() throws Exception {
+        int startESCount = eventStreamManagerAdminServiceClient.getEventStreamCount();
+        int startEPCount = eventPublisherAdminServiceClient.getActiveEventPublisherCount();
+
+        //Add StreamDefinition
+        String streamDefinitionAsString = getJSONArtifactConfiguration("outputflows/sample0052", "org.wso2.event.statistics.stream_1.0.0.json");
+        eventStreamManagerAdminServiceClient.addEventStreamAsString(streamDefinitionAsString);
+        Assert.assertEquals(eventStreamManagerAdminServiceClient.getEventStreamCount(), startESCount + 1);
+
+        //Add Text Logger
+        String eventPublisherConfig = getXMLArtifactConfiguration("outputflows/sample0052", "logger.xml");
+        eventPublisherAdminServiceClient.addEventPublisherConfiguration(eventPublisherConfig);
+        Assert.assertEquals(eventPublisherAdminServiceClient.getActiveEventPublisherCount(), startEPCount + 1);
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto.setAttributeValues(new String[]{"199008131245", "false", "400", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto2 = new EventDto();
+        eventDto2.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto2.setAttributeValues(new String[]{"199008131245", "false", "401", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto3 = new EventDto();
+        eventDto3.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto3.setAttributeValues(new String[]{"199008131245", "false", "403", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        eventSimulatorAdminServiceClient.sendEvent(eventDto);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto2);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto3);
+
+        eventStreamManagerAdminServiceClient.removeEventStream("org.wso2.event.statistics.stream", "1.0.0");
+        eventPublisherAdminServiceClient.removeInactiveEventPublisherConfiguration("logger.xml");
+
+        Thread.sleep(2000);
+
+    }
+
+    @Test(groups = {"wso2.cep"}, description = "Testing XML Logger")
+    public void loggerXMLTestScenario() throws Exception {
+        int startESCount = eventStreamManagerAdminServiceClient.getEventStreamCount();
+        int startEPCount = eventPublisherAdminServiceClient.getActiveEventPublisherCount();
+
+        //Add StreamDefinition
+        String streamDefinitionAsString = getJSONArtifactConfiguration("outputflows/sample0053", "org.wso2.event.statistics.stream_1.0.0.json");
+        eventStreamManagerAdminServiceClient.addEventStreamAsString(streamDefinitionAsString);
+        Assert.assertEquals(eventStreamManagerAdminServiceClient.getEventStreamCount(), startESCount + 1);
+
+        //Add Text Logger
+        String eventPublisherConfig = getXMLArtifactConfiguration("outputflows/sample0053", "logger.xml");
+        eventPublisherAdminServiceClient.addEventPublisherConfiguration(eventPublisherConfig);
+        Assert.assertEquals(eventPublisherAdminServiceClient.getActiveEventPublisherCount(), startEPCount + 1);
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto.setAttributeValues(new String[]{"199008131245", "false", "500", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto2 = new EventDto();
+        eventDto2.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto2.setAttributeValues(new String[]{"199008131245", "false", "501", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto3 = new EventDto();
+        eventDto3.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto3.setAttributeValues(new String[]{"199008131245", "false", "503", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        eventSimulatorAdminServiceClient.sendEvent(eventDto);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto2);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto3);
+
+        eventStreamManagerAdminServiceClient.removeEventStream("org.wso2.event.statistics.stream", "1.0.0");
+        eventPublisherAdminServiceClient.removeInactiveEventPublisherConfiguration("logger.xml");
+
+        Thread.sleep(2000);
+
+    }
+
+    @Test(groups = {"wso2.cep"}, description = "Testing XML mapping Logger")
+    public void loggerXMLWithMappingTestScenario() throws Exception {
+        int startESCount = eventStreamManagerAdminServiceClient.getEventStreamCount();
+        int startEPCount = eventPublisherAdminServiceClient.getActiveEventPublisherCount();
+
+        //Add StreamDefinition
+        String streamDefinitionAsString = getJSONArtifactConfiguration("outputflows/sample0054", "org.wso2.event.statistics.stream_1.0.0.json");
+        eventStreamManagerAdminServiceClient.addEventStreamAsString(streamDefinitionAsString);
+        Assert.assertEquals(eventStreamManagerAdminServiceClient.getEventStreamCount(), startESCount + 1);
+
+        //Add Text Logger
+        String eventPublisherConfig = getXMLArtifactConfiguration("outputflows/sample0054", "logger.xml");
+        eventPublisherAdminServiceClient.addEventPublisherConfiguration(eventPublisherConfig);
+        Assert.assertEquals(eventPublisherAdminServiceClient.getActiveEventPublisherCount(), startEPCount + 1);
+
+        EventDto eventDto = new EventDto();
+        eventDto.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto.setAttributeValues(new String[]{"199008131245", "false", "500", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto2 = new EventDto();
+        eventDto2.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto2.setAttributeValues(new String[]{"199008131245", "false", "501", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        EventDto eventDto3 = new EventDto();
+        eventDto3.setEventStreamId("org.wso2.event.statistics.stream:1.0.0");
+        eventDto3.setAttributeValues(new String[]{"199008131245", "false", "503", "temperature", "23.45656", "7.12324", "100.34", "23.4545"});
+
+        eventSimulatorAdminServiceClient.sendEvent(eventDto);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto2);
+        Thread.sleep(1000);
+        eventSimulatorAdminServiceClient.sendEvent(eventDto3);
+
+        eventStreamManagerAdminServiceClient.removeEventStream("org.wso2.event.statistics.stream", "1.0.0");
+        eventPublisherAdminServiceClient.removeInactiveEventPublisherConfiguration("logger.xml");
+
+        Thread.sleep(2000);
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void destroy() throws Exception {
+        super.cleanup();
+    }
+}
---
+++ modules/samples/artifacts/0053/ReadMe.txt
@@ -0,0 +1,7 @@
+This sample uses
+
+EventReceiver:  -
+EventPublisher: logger/XML with default mapping
+
+Producers:      event-simulator
+Consumers:      -
