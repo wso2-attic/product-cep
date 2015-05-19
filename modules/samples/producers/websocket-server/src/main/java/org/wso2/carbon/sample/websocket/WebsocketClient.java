38989e2e6c3d77f506d1d6131ab35e6661060126
dilini <dilini@wso2.com>
2015-05-20 02:34:38 +0530

    Adding sample 0019: Websocket

--- modules/samples/producers/websocket-server/src/main/java/org/wso2/carbon/sample/websocket/WebsocketClient.java
+++ modules/samples/producers/websocket-server/src/main/java/org/wso2/carbon/sample/websocket/WebsocketClient.java
@@ -1,18 +1,21 @@
 /*
-* Copyright 2004,2005 The Apache Software Foundation.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.sample.websocket;
 
 import org.eclipse.jetty.websocket.api.RemoteEndpoint;
@@ -52,7 +55,7 @@ public class WebsocketClient {
         try {
             subscriber.sendString("{\"event\": {\"metaData\":{\"timestamp\": 4354643,\"isPowerSaverEnabled\": false,\"sensorId\": 701,\"sensorName\": temperature},\"correlationData\": {\"longitude\": 4.504343,\"latitude\": 20.44345},\"payloadData\": {\"humidity\": 2.3,\"sensorValue\": 4.504343}}}");
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         } catch (Throwable t){
             t.printStackTrace();
         }
