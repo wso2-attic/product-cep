98856f8e099d9e6d75befc96ecc4f084d37a61f2
ramindu90 <ramindus@wso2.com>
2015-09-04 16:18:33 +0530

    Changing logger to use apache commons logging and updating license header in integration tests

--- modules/integration/tests-integration/tests/src/test/java/org/wso2/carbon/integration/test/client/HttpEventPublisherClient.java
+++ modules/integration/tests-integration/tests/src/test/java/org/wso2/carbon/integration/test/client/HttpEventPublisherClient.java
@@ -1,22 +1,26 @@
 /*
  * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.integration.test.client;
 
 import org.apache.axiom.om.util.Base64;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -34,7 +38,7 @@ import java.util.regex.Matcher;
  * HttpEventPublisherClient client reads a text file with multiple events in different formats and post it to the given url.
  */
 public class HttpEventPublisherClient {
-	private static Logger log = Logger.getLogger(HttpEventPublisherClient.class);
+	private static Log log = LogFactory.getLog(HttpEventPublisherClient.class);
 
 	public static void publish(String url, String username, String password, String testCaseFolderName, String dataFileName) {
 		log.info("Starting WSO2 HttpEventPublisher Client");
