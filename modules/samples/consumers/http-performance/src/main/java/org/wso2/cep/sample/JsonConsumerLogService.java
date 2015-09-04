46132e5dad5063116ab540b255b51af279b002f8
ramindu90 <ramindus@wso2.com>
2015-09-04 12:31:03 +0530

    Adding changes to getting timestamp in received event

--- modules/samples/consumers/http-performance/src/main/java/org/wso2/cep/sample/JsonConsumerLogService.java
+++ modules/samples/consumers/http-performance/src/main/java/org/wso2/cep/sample/JsonConsumerLogService.java
@@ -1,20 +1,23 @@
-package org.wso2.cep.sample;/*
+/*
  * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
-import com.jayway.jsonpath.JsonPath;
+package org.wso2.cep.sample;
+
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
@@ -25,8 +28,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.DecimalFormat;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonConsumerLogService extends HttpServlet {
 
@@ -36,13 +40,14 @@ public class JsonConsumerLogService extends HttpServlet {
     private static int elapsedCount = 50000;
     private static AtomicLong eventCount = new AtomicLong(0);
     private static long lastTime;
-
     private static AtomicLong latency = new AtomicLong(0);
+    private Pattern eventPattern;
 
     public void init() throws ServletException {
         log.info("Logger service initiated");
         eventCount.set(0);
         lastTime = System.currentTimeMillis();
+        eventPattern = Pattern.compile("(\"timestamp\":(\\d+))");
     }
 
     public void doGet(HttpServletRequest request,
@@ -57,9 +62,8 @@ public class JsonConsumerLogService extends HttpServlet {
         this.inputStreamToString(request.getInputStream());
     }
 
-    private String inputStreamToString(InputStream in) throws IOException {
+    private void inputStreamToString(InputStream in) throws IOException {
         long receivedTime = System.currentTimeMillis();
-
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         byte[] buff = new byte[1024];
         int i;
@@ -67,34 +71,26 @@ public class JsonConsumerLogService extends HttpServlet {
             out.write(buff, 0, i);
         }
         out.close();
+        String receivedMessage = out.toString();
+        Matcher eventPatternMatcher = eventPattern.matcher(receivedMessage);
+        if (eventPatternMatcher.find()) {
+            long sentTime = Long.parseLong(eventPatternMatcher.group(2));
+            latency.addAndGet(receivedTime - sentTime);
+            eventCount.addAndGet(1);
+            if (eventCount.get() % elapsedCount == 0) {
+                long currentTime = System.currentTimeMillis();
+                long elapsedTime = currentTime - lastTime;
+                double throughputPerSecond = (((double) elapsedCount) / elapsedTime) * 1000;
+                lastTime = currentTime;
+                log.info("Sent " + elapsedCount + " sensor events in " + elapsedTime +
+                         " milliseconds with total throughput of " + decimalFormat.format(throughputPerSecond) +
+                         " events per second.");
+                log.info("Receiving Latency: " + (double) latency.get() / elapsedCount);
+                latency = new AtomicLong(0);
 
-        //TODO Do we need to use json path to get the timestamp, string manipulations might be faster
-        JsonPath jsonPath = JsonPath.compile("$.event.metaData");
-        Map<Object, Object> eventMap = jsonPath.read(out.toString());
-
-        long sentTime = Long.parseLong(eventMap.get("timestamp").toString());
-
-        latency.addAndGet(receivedTime - sentTime);
-
-        eventCount.addAndGet(1);
-
-        if (eventCount.get() % elapsedCount == 0) {
-
-            long currentTime = System.currentTimeMillis();
-            long elapsedTime = currentTime - lastTime;
-            double throughputPerSecond = (((double)elapsedCount) / elapsedTime) * 1000;
-            lastTime = currentTime;
-
-            log.info("Sent " + elapsedCount + " sensor events in " + elapsedTime +
-                    " milliseconds with total throughput of " + decimalFormat.format(throughputPerSecond) +
-                    " events per second.");
-
-            log.info("Receiving Latency: " + (double)latency.get() / elapsedCount);
-            latency = new AtomicLong(0);
-
+            }
+        } else {
+            log.error("unable to extract timestamp from received event");
         }
-
-        return out.toString();
     }
-
 }
